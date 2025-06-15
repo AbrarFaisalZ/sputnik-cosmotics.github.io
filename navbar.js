e23ef19ada59898a2099093b433ce32c5db0cd6f
AbrarFaisalZ <abrarfaisal95432@gmail.com>
2025-06-15 20:22:46 +0600

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -5,10 +5,10 @@ export function renderNavbar() {
       <div style="font-weight:bold;font-size:1.2rem;">🚀 Sputnik Cosmotics</div>
       <div style="display:flex;gap:1rem;">
         <a href="index.html" style="color:white;text-decoration:none;">Home</a>
-        <a href="about.html" style="color:white;text-decoration:none;">About</a>
-        <a href="blog.html" style="color:white;text-decoration:none;">Blog</a>
-        <a href="journal.html style="color:green;text-decoration:none;>Journal</a>
-        <a id="profile-link" href="profile.html" style="color:white;text-decoration:none;">Profile</a>
+        <a href="about.html" style="color:gold;text-decoration:none;">About</a>
+        <a href="blog.html" style="color:maroon;text-decoration:none;">Blog</a>
+        <a href="journal.html" style="color:green;text-decoration:none;>Journal</a>
+        <a id="profile-link" href="profile.html" style="color:orange;text-decoration:none;">Profile</a>
       </div>
     </nav>
   `;
